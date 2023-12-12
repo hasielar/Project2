6010faa6c39b6e1befe1d63b803b5533c6de17f3
hasielar <144086255+hasielar@users.noreply.github.com>
2023-12-12 10:36:33 +0800

    Update Youtubers_help.py

--- Youtubers_help.py
+++ Youtubers_help.py
@@ -53,7 +53,15 @@ def main():
     top_10_views = selected_videos.nlargest(10, 'views')[['title', 'views', 'likes', 'engagement']]
     st.write(top_10_views)
 
+    # Line chart for video views over time for the selected channel
+    st.header('Video Views Over Time')
+    st.write(f"Video views over time for **{selected_channel}**")
+    selected_videos['published_date'] = pd.to_datetime(selected_videos['published_date'])
+    views_over_time = selected_videos.groupby(selected_videos['published_date'].dt.date)['views'].sum()
 
+    # Handle infinite values in views_over_time
+    views_over_time = views_over_time.replace([np.inf, -np.inf], np.nan)
+    views_over_time = views_over_time.dropna()
 
     # Create the Matplotlib figure for video views
     fig_views, ax_views = plt.subplots(figsize=(12, 6))
